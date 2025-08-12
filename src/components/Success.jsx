b4402e74fd73ca2bccb5f17a78de75913748c4eb
Kaantuzer <kaantzer@hotmail.com>
2025-08-12 12:55:46 +0300

    success screen renewed

--- src/components/Success.jsx
+++ src/components/Success.jsx
@@ -1,4 +1,4 @@
-import './Success.css'
+
 import { addMinutes, format, formatDistance, formatRelative, subDays } from 'date-fns'
 
 
@@ -7,24 +7,25 @@ function Success(props) {
   const currentDate = new Date();
   const estimatedTime1 = addMinutes(currentDate, 20);
   const estimatedTime2 = addMinutes(currentDate, 40);
-  return (
-    <div className="success maine">
-      <p className='success-message-up'>Lezzetin Yolda</p>
-      <p className='success-message'>Sipariş Alındı</p>
-      <hr className='hr' />
-      {/* <Summary formData={formData} total={total} selectedTotal={selectedTotal} /> */}
-      <h2>{selectedProduct.ad ? <span style={{ color: '#fff' }}>{selectedProduct.ad}</span> : "Ürün"} sipariş verildi!</h2>
 
-      <div className="summary-order">
+  return (
+    <div className="bg-projectRed h-screen text-center p-6 text-white flex flex-col justify-center items-center">
+      <p className='text-buttonYellow text-3xl font-light'>Lezzetin Yolda</p>
+      <p className='text-6xl font-extralight'>Sipariş Alındı</p>
+      <hr className='w-[80%] md:w-[35%] mx-auto mb-8 mt-6 border-gray-200' />
+      {/* <Summary formData={formData} total={total} selectedTotal={selectedTotal} /> */}
+      <h2 className='text-2xl'>{selectedProduct.name}</h2>
+
+      <div className="font-semibold text-white border p-10 text-left w-full lg:w-[700px] mx-auto mt-5 rounded-lg">
 
         <p>Boyut: {responseData.size}</p>
         <p>Hamur: {responseData.crustThickness}</p>
         <p>Ek Malzemeler: {responseData.malzeme.join(', ')}</p>
         <p>Not: {responseData.orderNote}</p>
         <p>Adet: {responseData.quantity}</p>
-        <p className='middle'>Seçilen Toplam: <br /> {selectedTotal}</p>
-        <p className='middle'>Toplam: <br /> {total}</p>
-        <p>Tahmini Teslim Saati: <br /> <span style={{ color: '#fff' }}>{format(estimatedTime1, 'HH:mm')} - {format(estimatedTime2, 'HH:mm')}</span></p>
+        <p className=''>Seçilen Toplam:  <span className='font-bold'>{selectedTotal}₺</span></p>
+        <p className=''>Toplam:  <span className='font-bold'>{total}₺</span></p>
+        <p className='text-right mt-6 md:mt-0'>Tahmini Teslim Saati: <br /> <span className='font-bold'>{format(estimatedTime1, 'HH:mm')} - {format(estimatedTime2, 'HH:mm')}</span></p>
 
       </div>
 
